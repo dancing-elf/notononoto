a0db94e6a72c018b2b9e7cd7a14e6e848858181b
Anton Saraev <antonysaraev@gmail.com>
2016-09-02 20:10:43 +0300

    Switched to gradle infrastructure

---
+++ client/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const PATHS = {
+    src: path.join(__dirname, 'src'),
+    build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+    entry: {
+        app: PATHS.src
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loaders: ['eslint'],
+                include: [PATHS.src]
+            }
+        ],
+        loaders: [
+            {
+                loaders: ['babel-loader'],
+                include: [PATHS.src],
+                test: /\.js$/,
+                plugins: ['transform-runtime']
+            }
+        ]
+    }
+};
