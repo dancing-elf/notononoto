3037a71937ad79a91f435f330820f7afd9b53d5d
Anton Saraev <antonysaraev@gmail.com>
2016-11-06 21:05:36 +0300

    Fix orientdb warning

--- dist/src/main/dist/bin/run.sh
+++ dist/src/main/dist/bin/run.sh
@@ -2,6 +2,9 @@
 
 NOTO_HOME=..
 
+# set MaxDirectMemorySize for orientdb
+# see http://orientdb.com/docs/2.1/Release-2.2.0.html
 java -classpath "$NOTO_HOME/lib/*" \
      -Dlogback.configurationFile="$NOTO_HOME/conf/logback.xml" \
+     -XX:MaxDirectMemorySize=512g \
      com.notononoto.Notononoto "$NOTO_HOME"
